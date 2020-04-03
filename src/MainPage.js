f175efe52105fecbeb384854d1ab135f456c9708
Olga Zhukova <zhukova.o.m@yandex.ru>
2020-04-03 10:56:57 +0500

    Набросок страницы, с которой можно будет начать новую игру или присоединиться к существующей

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,15 +1,48 @@
 import {Link} from "react-router-dom";
 import React from "react";
+import PropTypes from 'prop-types';
+import Gapped from "@skbkontur/react-ui/Gapped";
+import Button from "@skbkontur/react-ui/Button";
+import Input from "@skbkontur/react-ui/Input";
 
-export const MainPage = () => {
+const newGameId = '1234-abcd';
+
+const JoinGame = () => {
+    const gameIdPlaceholder = 'Идентификатор игры';
+    return (
+        <form className={'vertical'} action={''} method={'XXX'}>
+            <Input placeholder={gameIdPlaceholder} required/>
+            <Button type={'submit'}>Присоединиться</Button>
+        </form>
+    );
+};
+
+const InitGame = () => (
+    <Gapped gap={20} vertical>
+        <Button
+            onClick={() => alert(`Идентификатор игры: ${newGameId}.\nОтправьте его друзьям и они смогут присоединиться к вам!`)}>Начать
+            новую игру</Button>
+        <JoinGame/>
+    </Gapped>
+);
+
+export const MainPage = ({loggedOn = false}) => {
     return (
         <div className={'centered'}>
             <header>
                 Online Activity
             </header>
 
-            <Link to='/registration'>Регистрация</Link>
+            {loggedOn
+                ? <InitGame/>
+                : <Link to='/registration'>Регистрация</Link>}
 
         </div>
     );
 };
+
+MainPage.propTypes = {
+    loggedOn: PropTypes.bool
+};
+
+export const LoggedOnPage = () => <MainPage loggedOn/>;
--- src/Routing.js
+++ src/Routing.js
@@ -1,13 +1,17 @@
 import {Link, Route, Switch} from "react-router-dom";
 import React from "react";
-import {MainPage} from "./MainPage";
+import {LoggedOnPage, MainPage} from "./MainPage";
 import {Registration} from "./Registration";
+
+const registrationPage = '/registration';
+const homePage = '/home';
 
 export const Navigation = () => (
     <nav>
         <ul>
             <li><Link to='/'>Main</Link></li>
-            <li><Link to='/registration'>Регистрация</Link></li>
+            <li><Link to={registrationPage}>Регистрация</Link></li>
+            <li><Link to={homePage}>После регистрации</Link></li>
         </ul>
     </nav>
 );
@@ -16,7 +20,8 @@ export const Main = () => (
     <main>
         <Switch>
             <Route exact path='/' component={MainPage}/>
-            <Route exact path='/registration' component={Registration}/>
+            <Route exact path={registrationPage} component={Registration}/>
+            <Route exact path={homePage} component={LoggedOnPage}/>
         </Switch>
     </main>
 );
